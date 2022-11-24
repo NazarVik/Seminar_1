49240afb1b75e00de8b34c98c89da1d5b320c57e
NazarVik <Nazarov_dev@icloud.com>
2022-11-24 21:45:33 +0300

    работа на семинаре

---
+++ seminar2/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int randomDigit = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + randomDigit);
+
+int ostatok = randomDigit % 10;
+Console.WriteLine("Последняя цифра: " + ostatok);
+
+ int firstDigit = randomDigit / 10;
+ Console.WriteLine("Первая цифра " + firstDigit);
+
+ int max - firstDigit;
+
+ if (ostatok > firstDigit)
+    max = ostatok;
