1a805ff4f54883e58805f501b9ced4a4b3fe644c
NazarVik <Nazarov_dev@icloud.com>
2022-11-24 22:04:25 +0300

    бдопрсвлоч

--- seminar2/task2/Program.cs
+++ seminar2/task2/Program.cs
@@ -10,11 +10,17 @@
 // 16, 4 -> кратно
 // ___________________________
 
-Console.Clear();
-Console.Write("введите число ");
-int number =  int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.Write("введите число ");
+// int number =  int.Parse(Console.ReadLine());
 
-if ((number % 7 == 0 ) && (number % 23 == 0)) Console.WriteLine(number + " кратно 7 и 23");
-else  Console.WriteLine(number + " не кратно 7 и 23");
+// if ((number % 7 == 0 ) && (number % 23 == 0)) Console.WriteLine(number + " кратно 7 и 23");
+// else  Console.WriteLine(number + " не кратно 7 и 23");
 
 
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
